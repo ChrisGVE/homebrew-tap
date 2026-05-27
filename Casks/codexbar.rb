19fc36d7c97c7bdeebf80b7f785d85433b7524b1
Christian C. Berclaz <christian.berclaz@mac.com>
2026-05-27 19:43:31 +0200

    cask: update codexbar sha256 for notarized build

--- Casks/codexbar.rb
+++ Casks/codexbar.rb
@@ -1,6 +1,6 @@
 cask "codexbar" do
   version "0.29.2"
-  sha256 "60ef61b4205a3b70ec5087651084db2e606e3dbea287d8c55ea437cea407d392"
+  sha256 "983a9a7b1def60e9174af052009067d117f27a899606d10a176a42ba95020b9a"
 
   url "https://github.com/ChrisGVE/CodexBar/releases/download/v#{version}/CodexBar-x86_64-v#{version}.zip"
   name "CodexBar"
